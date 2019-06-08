d668757ff2e114ba2c7c097ee1380eb1d8e48505
Funky <yangenqings@gmail.com>
2019-06-09 07:28:11 +0800

    Update Gentrie.php

--- app/controllers/Gentrie.php
+++ app/controllers/Gentrie.php
@@ -19,6 +19,11 @@ class Gentrie extends Controller
                 trie_filter_store($trie, $item);
             }
         }
-        trie_filter_save($trie, $this->treeFilePath);
+        $res = trie_filter_save($trie, $this->treeFilePath);
+        if ($res) {
+            echo '树生成成功，<a href="/">前往过滤</a>';
+        } else {
+            echo '树生成失败，<a href="GenTrie/gen">重新生成</a>';
+        }
     }
 }
